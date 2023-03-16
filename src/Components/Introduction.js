8e810563e050ff7596d631dc20e6653f67f49e64
GabriellaPenev <gabriellapenev@gmail.com>
2023-03-16 11:46:05 -0400

    added functionality to show/hide intro modal on page load and button click

--- src/Components/Introduction.js
+++ src/Components/Introduction.js
@@ -1,40 +1,50 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 export default function Introduction() {
 
     const [introModal, setIntroModal] = useState(false); // show intro on page mount
 
     const introRef = useRef(null);
+    const introButton = useRef(null);
+
+    useEffect(() => {
+
+        let timer = setTimeout(() => showIntro(), 1400);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [])
 
     const showIntro = () => {
-        setIntroModal(!introModal)
-
-        if(introModal === true){
-            introRef.current.classList.add('slideOutIntro');
-            introRef.current.classList.remove('slideInIntro');
-            
-        } else {
-            introRef.current.classList.remove('slideOutIntro');
-            introRef.current.classList.add('slideInIntro');
-        }
+        setIntroModal(true)
+        introButton.current.classList.add('hideIntroButton')
+        introRef.current.classList.remove('slideOutIntro');
+        introRef.current.classList.add('slideInIntro');
     }
 
+    const closeIntro = () => {
+        setIntroModal(false)
+
+        introButton.current.classList.remove('hideIntroButton')
+        introRef.current.classList.add('slideOutIntro')
+        introRef.current.classList.remove('slideInIntro')
+
+    }
+    // if introModal is true, ie it's being shown then add like an opacity over everything else (overlay) to prevent it from being clicked - up in app.js
+
     return (
-
-
         <>
-
             <div className="wrapper">
-                    <button 
-                    className='introButton'
-                    onClick={() => showIntro()}>{introModal ? `close` : `what's this about?`}</button>
+                <button ref={introButton} className='introButton' onClick={() => showIntro()}>what's this about?</button>
 
-                <div 
-                ref={introRef}
-                className='intro'>
+                { introModal ? <div className="overlay"></div> : null }
+                <div ref={introRef} className='intro'>
+                    <button className='closeButton' onClick={() => closeIntro()}>x</button>
                     <h1>Globe Spinner</h1>
-                    <p>Wondering where to travel to next? Globe Spinner can help! Simply click the 'spin globe' and watch the globe spin to a random city. Once the map zooms into a city, click the 'Show City Info' button to find out more about it!</p>
+                    <p>Wondering where to travel to next? Click the 'Spin Globe' button and watch the globe spin to a random city in the world, then click the 'Show City Info' button to find out more about it!</p>
                 </div>
+
             </div>
 
         </>
