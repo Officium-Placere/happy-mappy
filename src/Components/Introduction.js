3269e680c6874469837939cece2fcd5de9a7a7ed
GabriellaPenev <gabriellapenev@gmail.com>
2023-03-15 22:31:11 -0400

    tweaked intro container on page load

--- src/Components/Introduction.js
+++ src/Components/Introduction.js
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react';
 
 export default function Introduction() {
 
-    const [introModal, setIntroModal] = useState(true); // show intro on page mount
+    const [introModal, setIntroModal] = useState(false); // show intro on page mount
 
     const introRef = useRef(null);
 
@@ -33,7 +33,7 @@ export default function Introduction() {
                 ref={introRef}
                 className='intro'>
                     <h1>Globe Spinner</h1>
-                    <p>Wondering where to travel to next? Globe Spinner can help! Simply click the 'spin globe' and watch the globe spin to a random city. To find out more about that city, click the 'Show City Info' button!</p>
+                    <p>Wondering where to travel to next? Globe Spinner can help! Simply click the 'spin globe' and watch the globe spin to a random city. Once the map zooms into a city, click the 'Show City Info' button to find out more about it!</p>
                 </div>
             </div>
 
