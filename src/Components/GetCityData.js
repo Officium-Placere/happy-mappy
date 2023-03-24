dc0bdac58e8c75b4dd2e44a02b7e8610e72cf005
GabriellaPenev <gabriellapenev@gmail.com>
2023-03-24 09:41:38 -0400

    added try catch error handling to getAPIData async function

--- src/Components/GetCityData.js
+++ src/Components/GetCityData.js
@@ -50,139 +50,161 @@ export default function GetCityData({ map, trigger, showCityData }) {
 
             async function getApiData() {
 
-                const firstAPI = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode?latitude=${center.lat}&longitude=${center.lng}&localityLanguage=en&key=bdc_e3a41bcc2937431191cc18382f3d5492`)
-                    .then(response => response.json())
-                    .then(data => data)
+                try {
+
+
+                    const firstAPI = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode?latitude=${center.lat}&longitude=${center.lng}&localityLanguage=en&key=bdc_e3a41bcc2937431191cc18382f3d5492`)
+                        .then(response => response.json())
+                        .then(data => data)
+                        .catch((error) => {
+                            throw new Error(`Error fetching data from firstAPI: ${error.message}`);
+                        });
 
                     const cityObj = []
-                firstAPI.localityInfo.administrative.forEach(poiObj => {
-                    if (poiObj.name === firstAPI.city) {
-                        cityObj.push(poiObj)
-                    }
-                })
-                // if city isn't in administrative property, check informative property, do same loop:
-                if (cityObj.length === 0) {
-                    firstAPI.localityInfo.informative.forEach(poiObj => {
+                    firstAPI.localityInfo.administrative.forEach(poiObj => {
                         if (poiObj.name === firstAPI.city) {
                             cityObj.push(poiObj)
                         }
                     })
-                }
+                    // if city isn't in administrative property, check informative property, do same loop:
+                    if (cityObj.length === 0) {
+                        firstAPI.localityInfo.informative.forEach(poiObj => {
+                            if (poiObj.name === firstAPI.city) {
+                                cityObj.push(poiObj)
+                            }
+                        })
+                    }
 
-                let city
-                let id = ''
-                cityObj.forEach(obj => {
-                    if (obj.description) { // some of the cityObj objects do not have description properties, so only if they all do, then go through this:
-                        if (obj.description.includes('city')) {
-                            city = obj
-                        } else if (obj.description.includes('capital')) {
-                            city = obj
-                        } else if (obj.description.includes('municipality')) {
-                            city = obj
-                        } else if (obj.description.includes('metropolis')) {
-                            city = obj
+                    let city
+                    let id = ''
+                    cityObj.forEach(obj => {
+                        if (obj.description) { // some of the cityObj objects do not have description properties, so only if they all do, then go through this:
+                            if (obj.description.includes('city')) {
+                                city = obj
+                            } else if (obj.description.includes('capital')) {
+                                city = obj
+                            } else if (obj.description.includes('municipality')) {
+                                city = obj
+                            } else if (obj.description.includes('metropolis')) {
+                                city = obj
+                            }
+                            else city = false
                         }
                         else city = false
+                    })
+
+                    if (city === false) {
+                        city = cityObj[0]
                     }
-                    else city = false
-                })
 
-                if (city === false) {
-                    city = cityObj[0]
-                }
-
-                id = city.wikidataId;
-                const secondAPI = await fetch(`https://www.wikidata.org/w/api.php?action=wbgetentities&origin=*&ids=${id}&sitefilter=enwiki&format=json`)
-                    .then(response => response.json())
-                    .then(data => data)
+                    id = city.wikidataId;
+                    const secondAPI = await fetch(`https://www.wikidata.org/w/api.php?action=wbgetentities&origin=*&ids=${id}&sitefilter=enwiki&format=json`)
+                        .then(response => response.json())
+                        .then(data => data)
+                        .catch((error) => {
+                            throw new Error(`Error fetching data from secondAPI: ${error.message}`);
+                        });
 
                     const wikiTitle = secondAPI.entities[id].sitelinks.enwiki.title
 
                     setCityName(wikiTitle) //SET STATE 
 
-                const thirdAPI = await fetch(`https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${wikiTitle}&origin=*`)
-                    .then(response => response.json())
-                    .then(data => data)
+                    const thirdAPI = await fetch(`https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${wikiTitle}&origin=*`)
+                        .then(response => response.json())
+                        .then(data => data)
+                        .catch((error) => {
+                            throw new Error(`Error fetching data from thirdAPI: ${error.message}`);
+                        });
 
-                const keys = Object.keys(thirdAPI.query.pages)[0]
-                const wkExtract = (thirdAPI.query.pages[keys].extract)
-                // save first 3 sentences and remove '(listen)' link from the wiki extract
-                const listenRegex = (/\(listen\)/g);
-                const wkBlurb = wkExtract.match(/[^.]*.[^.]*.[^.]*./)[0].replace(listenRegex, '');
+                    const keys = Object.keys(thirdAPI.query.pages)[0]
+                    const wkExtract = (thirdAPI.query.pages[keys].extract)
+                    // save first 3 sentences and remove '(listen)' link from the wiki extract
+                    const listenRegex = (/\(listen\)/g);
+                    const wkBlurb = wkExtract.match(/[^.]*.[^.]*.[^.]*./)[0].replace(listenRegex, '');
 
-                setWikiBlurb(wkBlurb) //SET STATE 
+                    setWikiBlurb(wkBlurb) //SET STATE 
 
-                // fetch image from wiki article
-                const fourthAPI = await fetch(`https://en.wikipedia.org/w/api.php?action=query&origin=%2A&pithumbsize=800&prop=pageimages&titles=${wikiTitle}&format=json`)
-                    .then(response => response.json())
-                    .then(data => data)
+                    // fetch image from wiki article
+                    const fourthAPI = await fetch(`https://en.wikipedia.org/w/api.php?action=query&origin=%2A&pithumbsize=800&prop=pageimages&titles=${wikiTitle}&format=json`)
+                        .then(response => response.json())
+                        .then(data => data)
+                        .catch((error) => {
+                            throw new Error(`Error fetching data from fourthAPI: ${error.message}`);
+                        });
 
-                const wkImageKey = Object.keys(fourthAPI.query.pages)[0]
-                const wkImage = (fourthAPI.query.pages[wkImageKey].thumbnail.source)
+                    const wkImageKey = Object.keys(fourthAPI.query.pages)[0]
+                    const wkImage = (fourthAPI.query.pages[wkImageKey].thumbnail.source)
 
-                setWikiPic(wkImage) //SET STATE 
+                    setWikiPic(wkImage) //SET STATE 
 
-                let cityASCII = cityNum.properties['city-ascii'].toLowerCase().split(' ').join('-')
+                    let cityASCII = cityNum.properties['city-ascii'].toLowerCase().split(' ').join('-')
 
-                const teleportImg = await fetch(`https://api.teleport.org/api/urban_areas/slug:${cityASCII}/images/`)
-                    .then(response => response.json())
-                    .then(data => data);
-                const tpImage = teleportImg.photos[0].image.mobile
+                    const teleportImg = await fetch(`https://api.teleport.org/api/urban_areas/slug:${cityASCII}/images/`)
+                        .then(response => response.json())
+                        .then(data => data)
+                        .catch((error) => {
+                            throw new Error(`Error fetching data from teleportIMG API: ${error.message}`);
+                        });
 
-                setTpPic(tpImage) //SET STATE 
+                    const tpImage = teleportImg.photos[0].image.mobile
 
-                const teleportBlurb = await fetch(`https://api.teleport.org/api/urban_areas/slug:${cityASCII}/scores/`)
-                    .then(response => response.json())
-                    .then(data => data);
+                    setTpPic(tpImage) //SET STATE 
 
-                const citySummary = teleportBlurb.summary;
-                const cityRanking = teleportBlurb.categories
-                const cityRank = cityRanking.map((category) => {
-                    return `${category.name}: ${category.score_out_of_10.toFixed(2)} `
-                })
+                    const teleportBlurb = await fetch(`https://api.teleport.org/api/urban_areas/slug:${cityASCII}/scores/`)
+                        .then(response => response.json())
+                        .then(data => data)
+                        .catch((error) => {
+                            throw new Error(`Error fetching data from teleportBlurb API: ${error.message}`);
+                        });
 
-                setTpMetrics(cityRank) //SET STATE 
+                    const citySummary = teleportBlurb.summary;
+                    const cityRanking = teleportBlurb.categories
+                    const cityRank = cityRanking.map((category) => {
+                        return `${category.name}: ${category.score_out_of_10.toFixed(2)} `
+                    })
 
-                // regex to select category title and metric out of string and push them into a data object
-                const titleRegex = /^.*?(?=:)/gm;
-                const metricRegex = /(?<=: ).*/gm;
-                const graphData = [];
+                    setTpMetrics(cityRank) //SET STATE 
 
-                cityRank.forEach((metric) => {
-                    graphData.push({ title: metric.match(titleRegex), metric: parseFloat(metric.match(metricRegex)) })
-                })
+                    // regex to select category title and metric out of string and push them into a data object
+                    const titleRegex = /^.*?(?=:)/gm;
+                    const metricRegex = /(?<=: ).*/gm;
+                    const graphData = [];
 
-                // SET STATE FOR CHART JS GRAPH
-                setChartData({
-                    labels: graphData.map((data) => data.title[0]),
-                    datasets: [
-                        {
-                            label: 'Score',
-                            data: graphData.map((data) => data.metric),
-                            backgroundColor: '#2f4d6b',
-                            borderColor: "white",
-                            borderWidth: 0.1,
-                            borderRadius: 25
-                        }
-                    ]
-                });
+                    cityRank.forEach((metric) => {
+                        graphData.push({ title: metric.match(titleRegex), metric: parseFloat(metric.match(metricRegex)) })
+                    })
 
-                // removes anything in between <i> tags (author byline for tpSummary):
-                const bylineRegex = /<i>(.*?)<\/i>/gs;
-                // removes html tags from teleport summary:
-                const htmlRegex = /(<([^>]+)>)/ig;
-                const tpSummary = citySummary.replace(bylineRegex, '').replace(htmlRegex, '');
+                    // SET STATE FOR CHART JS GRAPH
+                    setChartData({
+                        labels: graphData.map((data) => data.title[0]),
+                        datasets: [
+                            {
+                                label: 'Score',
+                                data: graphData.map((data) => data.metric),
+                                backgroundColor: '#2f4d6b',
+                                borderColor: "white",
+                                borderWidth: 0.1,
+                                borderRadius: 25
+                            }
+                        ]
+                    });
 
-                setTpBlurb(tpSummary) //SET STATE 
+                    // removes anything in between <i> tags (author byline for tpSummary):
+                    const bylineRegex = /<i>(.*?)<\/i>/gs;
+                    // removes html tags from teleport summary:
+                    const htmlRegex = /(<([^>]+)>)/ig;
+                    const tpSummary = citySummary.replace(bylineRegex, '').replace(htmlRegex, '');
+
+                    setTpBlurb(tpSummary) //SET STATE 
+                }
+
+                catch (error) {
+                    console.error(`Error in getApiData() async function: ${error.message}`);
+                    // Handle error or rethrow the error to the calling function
+                    throw error;
+                }
             }
-
             getApiData()
-
-
-            // try {
-            // } catch (error) {
-            //     alert(`Looks like there's an issue on our end! Try again later!`)
-            // }
         }
 
         // don't run on the initial render
@@ -209,7 +231,7 @@ export default function GetCityData({ map, trigger, showCityData }) {
                             className={showInfo ? 'city-info-container slideout-active' : 'city-info-container'}>
                             <div className="wrapper">
                                 <div className='city-info'>
-                                <button className='close-city-info-button' onClick={() => setShowInfo(false)}>x</button>
+                                    <button className='close-city-info-button' onClick={() => setShowInfo(false)}>x</button>
 
                                     <h2>{cityName}</h2>
                                     <p>{wikiBlurb}.. <a target="_blank" rel="noopener noreferrer" href={`https://en.wikipedia.org/wiki/${cityName}`}>see more</a></p >
@@ -223,7 +245,7 @@ export default function GetCityData({ map, trigger, showCityData }) {
                                     </div>
                                     {/* <h3>Teleport City Ranking</h3> */}
                                     <p>{tpBlurb}</p>
-                                    <Graph chartData={chartData}/>
+                                    <Graph chartData={chartData} />
                                     <p className='visually-hidden'>City Ranking per Category: Score out of 10: {tpMetrics}</p>
                                 </div >
                             </div >
